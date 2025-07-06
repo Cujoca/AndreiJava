6b047a27fe5f743c848eda76096975d6b8103985
Cujoca <andrei@razie.com>
2025-07-06 14:05:41 -0400

    Generated JavaDocs for Lab 05 located within DataStrucLab05/JavaDoc

---
+++ DataStrucLab05/JavaDoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"DoublyLinkedListGeneric"},{"p":"<Unnamed>","l":"DoublyLinkedListGeneric.Node"}];updateSearchResults();
